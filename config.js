5292a87362acdee1437f22a2f2086bd8fbebccb6
Vtr0 <icwt@rocketmail.com>
2024-11-27 15:52:50 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+var _DEFAULT_CFG = {xState: {speed:1.4,volume:1, muted:false,history:{currHis:0,hisList:[]}},
+cDBFilesEdt: {checkEdtDelay:21_600, edt:[110,110,114,100]}, //in seconds: 43_200, 86_400, edt:[101,101,101,101]}
+xDBFilesCfg: {len:4,toLoad:null}}; 
+var ssData = ssData || {
+	wildCartStr: "<*~~*>",
+	xBookGrp: [{grpWay:"Nhóm theo Bộ truyện",grp:[]},{grpWay:"Sắp xếp theo Người đọc",grp:[]},{grpWay:"Sắp xếp theo Tên truyện",grp:[]}]
+}
