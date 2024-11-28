24c7086569a77c999b55e6f87668d1a7c63ed322
Vtr0 <icwt@rocketmail.com>
2024-11-28 16:12:31 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var _DEFAULT_CFG = {xState: {speed:1.4,volume:1, muted:false,history:{currHis:0,hisList:[]}},
-cDBFilesEdt: {checkEdtDelay:21_600, edt:[110,110,114,100]}, //in seconds: 43_200, 86_400, edt:[101,101,101,101]}
+cDBFilesEdt: {checkEdtDelay:21_600, edt:[110,110,115,101]}, //in seconds: 43_200, 86_400, edt:[101,101,101,101]}
 xDBFilesCfg: {len:4,toLoad:null}}; 
 var ssData = ssData || {
 	wildCartStr: "<*~~*>",
