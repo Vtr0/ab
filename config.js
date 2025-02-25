8594753274e833fcfdc5155134a1bdce89b67f8e
Vtr0 <icwt@rocketmail.com>
2025-02-25 16:01:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var _DEFAULT_CFG = {xState: {speed:1.4,volume:1, muted:false,history:{currHis:0,hisList:[],books:{}}},
-cDBFilesEdt: {checkEdtDelay:21_600, edt:[111,115,118,123]}, //in seconds: 43_200, 86_400, edt:[101,101,101,101]}
+cDBFilesEdt: {checkEdtDelay:21_600, edt:[111,115,118,124]}, //in seconds: 43_200, 86_400, edt:[101,101,101,101]}
 xDBFilesCfg: {len:4,toLoad:null}}; 
 var ssData = ssData || {
 	wildCartStr: "<*~~*>",
