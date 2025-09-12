19e87d4601d1cbdc1f77b26e68e25885a3991d13
Vtr0 <icwt@rocketmail.com>
2025-09-12 16:48:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 var _DEFAULT_CFG = {xState: { audio:{speed:1.4,volume:1,muted:false},history:{currHis:0,hisList:[],books:{}} }, 
-cDBFilesEdt: {checkEdtDelay:21_600, edt:[101,102,101,102]}, //in seconds: 43_200, 86_400, edt:[101,101,101,101]}
+cDBFilesEdt: {checkEdtDelay:21_600, edt:[101,103,101,102]}, //in seconds: 43_200, 86_400, edt:[101,101,101,101]}
 xDBFilesCfg: {len:4,toLoad:null}}; 
 var ssData = ssData || {
 	wildCartStr: "<*~~*>",
 	xBookGrp: [{grpWay:"Nhóm theo Bộ truyện",grp:[]},{grpWay:"Sắp xếp theo Người đọc",grp:[]},{grpWay:"Sắp xếp theo Tên truyện",grp:[]}]
 }
+
