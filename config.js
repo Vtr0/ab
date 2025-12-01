82ca69ff1310e3def2a7aa7766ae519f8614815b
Vtr0 <icwt@rocketmail.com>
2025-12-01 08:52:34 +0700

    make dark to be the dafault theme

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var _DEFAULT_CFG = {xState: { audio:{speed:1.4,volume:1,muted:false},history:{currHis:0,hisList:[],books:{}} }, 
+var _DEFAULT_CFG = {xState: { audio:{speed:1.4,volume:1,muted:false},history:{currHis:0,hisList:[],books:{}} }, pageTheme:"dark", 
 cDBFilesEdt: {checkEdtDelay:21_600, edt:[101,101,101,101]}, //in seconds: 43_200, 86_400, edt:[101,101,101,101]}
 xDBFilesCfg: {len:4,toLoad:null}};
 var ssData = ssData || {
